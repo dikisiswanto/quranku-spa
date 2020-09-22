e54c5e61e275949b5ac8f83df37cf1d3a18616de
Diki Siswanto <dickyachmady@gmail.com>
2020-09-22 21:33:52 +0800

    Updates

---
+++ precache-manifest.f8da27756be5409124d27a91e1817a2a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "094b91a848788ec2d7a62b9197339b33",
+    "url": "/quranku-spa/index.html"
+  },
+  {
+    "revision": "952456d006cc29f85117",
+    "url": "/quranku-spa/static/css/main.ac281cc2.chunk.css"
+  },
+  {
+    "revision": "f13c9af28e453bf5a4bb",
+    "url": "/quranku-spa/static/js/2.bc19a78f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/quranku-spa/static/js/2.bc19a78f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "952456d006cc29f85117",
+    "url": "/quranku-spa/static/js/main.7d9b9383.chunk.js"
+  },
+  {
+    "revision": "c86e63aa8d11fe619fda",
+    "url": "/quranku-spa/static/js/runtime-main.5afccf48.js"
+  }
+]);
