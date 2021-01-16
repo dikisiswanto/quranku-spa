84c4397073f7b5f802bdd476b5e83dc0ed8f6d09
Diki Siswanto <dickyachmady@gmail.com>
2021-01-16 13:32:53 +0800

    enable purgecss

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,10 @@ module.exports = {
     // removeDeprecatedGapUtilities: true,
     purgeLayersByDefault: true,
   },
-  purge: ['./src/**/*.js', './src/*.js'],
+  purge: {
+    enabled: true,
+    content: ['./src/**/*.js', './src/*.js'],
+  },
   theme: {
     extend: {},
   },
